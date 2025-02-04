df5dffb9332334deb6221a5c3649158564e6afec
Fahimeh Pourhosseini <Fahimeh@Hanas-MBP.home>
2025-02-04 18:47:26 +0100

    handle misspelled dog breeds with difflib

--- breeds/views.py
+++ breeds/views.py
@@ -1,6 +1,7 @@
 import urllib.request
 import json
 import re
+import difflib
 from django.http import JsonResponse, HttpRequest
 
 
@@ -13,7 +14,28 @@ def list_all_breeds(request: HttpRequest) -> JsonResponse:
     return JsonResponse(data)
 
 
+VALID_BREEDS_URL = 'https://dog.ceo/api/breeds/list/all'
+
+
+def get_valid_breeds():
+    with urllib.request.urlopen(VALID_BREEDS_URL) as response:
+        data = json.loads(response.read().decode())
+    return list(data['message'].keys())
+
+
+VALID_BREEDS = get_valid_breeds()
+
+
 def list_breed_images(request: HttpRequest, breed: str) -> JsonResponse:
+
+    if breed not in VALID_BREEDS:
+        closest_matches = difflib.get_close_matches(breed, VALID_BREEDS, n=1)
+        if closest_matches:
+            closest_match = closest_matches[0]
+            return JsonResponse({'error': f'Did you mean: {closest_match}?'}, status=400)
+        else:
+            return JsonResponse({'error': 'Breed not found and no close matches available.'}, status=400)
+
     url = f'https://dog.ceo/api/breed/{breed}/images'
 
     with urllib.request.urlopen(url) as response:
@@ -47,5 +69,3 @@ def breed_images_from_random(request: HttpRequest) -> JsonResponse:
     breed_images_data = json.loads(breed_images_response.content)
 
     return JsonResponse(breed_images_data)
-
-
