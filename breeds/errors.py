045c0f6e0a4cf400509c3e9a1b62a1af211e305d
Fahimeh <f.prhssn@gmail.com>
2025-02-13 18:42:35 +0100

    add serializers

---
+++ breeds/errors.py
@@ -0,0 +1,6 @@
+class ClientError(Exception):
+    pass
+
+
+class ServiceError(Exception):
+    pass
---
+++ breeds/serializers.py
@@ -0,0 +1,40 @@
+
+from abc import ABC, abstractmethod
+from .models import DogImage, BreedImages, BreedList
+
+
+class Serializer(ABC):
+    @abstractmethod
+    def serialize(self):
+        pass
+
+
+class BreedImageListSerializer(Serializer):
+    def __init__(self, instance: BreedImages):
+        self.instance = instance
+
+    def serialize(self):
+        return {
+            'urls': self.instance.urls,
+        }
+
+
+class RandomImageSerializer(Serializer):
+    def __init__(self, instance: DogImage):
+        self.instance = instance
+
+    def serialize(self):
+        return {
+            'url': self.instance.url,
+            'breed': self.instance.breed
+        }
+
+
+class BreedListSerializer(Serializer):
+    def __init__(self, instance: BreedList):
+        self.instance = instance
+
+    def serialize(self):
+        return {
+            'breeds': self.instance.breeds
+        }
